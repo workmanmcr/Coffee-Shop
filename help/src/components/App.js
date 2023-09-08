84a6c84c3e416348c81b266128d5ec7bc922dead
workmanmcr <workmanmcr@gmail.com>
2023-09-08 08:42:37 -0700

    add goffee js

---
+++ help/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import MenuControl from "./MenuControl";
+
+function App(){
+  return(
+    <React.Fragment>
+      <Header />
+      <MenuControl />
+      <hr/>
+    </React.Fragment>
+  )
+}
+
+export default App;
